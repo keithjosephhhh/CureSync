4bf6d29e74b485ebaef4a96202cdd568b2dc2fe6
keithjosephhhh <Keithjoseph76@gmail.com>
2025-05-07 03:44:43 +0530

    Initial

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { DoctorContext } from '../context/DoctorContext'
+import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+
+  const { dToken, setDToken } = useContext(DoctorContext)
+  const { aToken, setAToken } = useContext(AdminContext)
+
+  const navigate = useNavigate()
+
+  const logout = () => {
+    navigate('/')
+    dToken && setDToken('')
+    dToken && localStorage.removeItem('dToken')
+    aToken && setAToken('')
+    aToken && localStorage.removeItem('aToken')
+  }
+
+  return (
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+      <div className='flex items-center gap-2 text-xs'>
+        <p onClick={() => navigate('/')} className="w-36 sm:w-40 cursor-pointer text-3xl font-extrabold text-blue-600 hover:text-white bg-white hover:bg-blue-600 px-3 py-1 rounded-lg transition-all duration-300">CureSync</p>
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
+      </div>
+      <button onClick={() => logout()} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+        <p onClick={() => navigate('/')} className="w-36 sm:w-40 cursor-pointer text-3xl font-extrabold text-blue-600 hover:text-white bg-white hover:bg-blue-600 px-3 py-1 rounded-lg transition-all duration-300">CureSync</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91-1234567890</li>
+            <li>AF1@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ CureSync.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
